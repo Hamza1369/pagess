12068012d5e017af863c58fd58b0fa13a41a74e8
zeeshan879 <sp17bsed@gmail.com>
2024-06-22 23:04:57 +0500

    ff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './components/home';
+import SecondPage from './components/secondPage';
 // import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
+      <SecondPage/>
     </div>
   );
 }
---
+++ src/components/secondPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+const hg = [
+    "https://paranoize.otomeserver.com/AllFrames/e001-1.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-2.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-3.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-4.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-5.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-6.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-7.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-8.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-9.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-10.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-11.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-12.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-13.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-14.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-15.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-16.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-17.png",
+    "https://paranoize.otomeserver.com/AllFrames/e001-18.png",
+];
+const SecondPage = () => {
+    return (
+        <div className="flex justify-center flex-col items-center w-full gap-5">
+            {hg.map((link) => (
+                <img src={link} alt="" className="min-w-[375px]  h-[350px] object-contain mx-auto" />
+            ))}
+        </div>
+    );
+};
+
+export default SecondPage;
